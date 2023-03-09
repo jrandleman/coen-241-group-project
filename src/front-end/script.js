06fc3022e530270c0d731558ea27d3948d29e28d
rahulyal <vyalavarthi@scu.edu>
2023-03-09 02:26:46 -0800

    working POST request frontend
    Now also hosted on a ec2 instance

--- src/front-end/script.js
+++ src/front-end/script.js
@@ -2,7 +2,6 @@ function compareData() {
   // get input player data
   var player = document.getElementById("player").value;
   var country = document.getElementById("country").value;
-
   // read player data from data.json
   fetch('data.json')
     .then(response => response.json())
@@ -17,23 +16,32 @@ function compareData() {
       // Check if the country matches
       if (playerData.country === country) {
         alert("Player and country match in the data!");
+        console.log(player,country);
+        fetch('https://98azzq6cyd.execute-api.us-east-1.amazonaws.com/default/schedulescraper', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              playername : player,
+              countryname : country
+            })
+          })
+          .then(response => {
+            return response.json();
+          })
+          .then(data => {
+            // handle the response from the Lambda function here
+            console.log(data);
+            alert('API call successful!');
+          })
+          .catch(error => {
+            console.error(error);
+            alert('API call failed!');
+          });
       } else {
         alert("Player found, but country does not match in the data!");
       }
     })
     .catch(error => console.error(error));
 }
-
-function makeApiCall() {
-      axios.get('https://98azzq6cyd.execute-api.us-east-1.amazonaws.com/default/schedulescraper')
-.then((response) => {
-  console.log(response.data);
-  alert('API call successful!');
-})
-.catch((error) => {
-  console.error(error);
-  alert('API call failed!');
-});
-
-    }
-
