39d191bd360f674a63e014c841a63fe520363438
Jordan Randleman <jordanran199@gmail.com>
2023-03-12 18:57:21 -0700

    Update index.js

--- src/cricket-api-nodejs-downloaded-html/index.js
+++ src/cricket-api-nodejs-downloaded-html/index.js
@@ -87,4 +87,35 @@ function main() {
     return livescore;
 }
 
-main();
+
+// main();
+
+
+
+
+
+
+export const handler = async (event, context) => {
+  try {
+    const results = main();
+    console.log(JSON.stringify(results,null,2));
+    return {
+        statusCode: 200,
+        body: JSON.stringify(results),
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*', // replace * with the allowed origin
+          'Access-Control-Allow-Headers': 'Content-Type',
+          'Access-Control-Allow-Methods': 'GET,OPTIONS,POST'
+        }
+    };
+} catch(err) {
+    console.error(`Couldn't parse cricbuzz! Error: ${err}`);
+    return {
+        statusCode: 500,
+        body: JSON.stringify({
+            message: 'Error parsing cricbuzz!'
+        })
+    };
+}
+};
